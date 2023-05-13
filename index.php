129566855194fc28295bc130aa82c642e12776eb
luizvelasco <luizvelasco@gmail.com>
2023-05-13 15:02:54 -0300

    index

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-<<<<<<< HEAD
 echo "<h1>Arquivos do curso de PHP Resursos Essenciais</h1>";
 
 echo "<br>";
-=======
-echo "<h1>Arquivos do curso de PHP Resursos Essenciais</h1>";
->>>>>>> 9ec26bc902d8b73cdb1dcbbe38bf34d5803b78e2
