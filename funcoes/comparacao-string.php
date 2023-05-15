33717bbfdc37b8c0b887998eec16070f4705a889
luizvelasco <luizvelasco@gmail.com>
2023-05-15 09:05:19 -0300

    comparação avançada de string

---
+++ funcoes/comparacao-string.php
@@ -0,0 +1,14 @@
+<?php
+
+var_dump('TreinaWeb' == 0);
+
+var_dump('11t' === 11);
+echo "<br>";
+
+if (strcmp('aa', 'aa') === 0){ //compara strings
+    echo "as strings são iguais<br>";
+}
+
+if (strcasecmp('Aa', 'aa') === 0){ //compara strings não levando em conta maiúscula ou minúscula
+    echo "as strings são iguais<br>";
+}
