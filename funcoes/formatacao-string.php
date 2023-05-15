b87bfb8aa61254efa44934df36b946aaf9d54323
luizvelasco <luizvelasco@gmail.com>
2023-05-15 08:39:30 -0300

    Funções para trabalhar com padrões de string

--- funcoes/formatacao-string.php
+++ funcoes/formatacao-string.php
@@ -7,9 +7,23 @@ echo 'Curso de', $curso, ' versao ', $versao;
 echo "<br>";
 echo 'Curso de'. $curso.  ' versao ' . $versao;
 echo "<br>";
-echo "Curso de. $curso  versão  $versao";
+echo "Curso de $curso  versão  $versao"; //mais aconselhável
 echo "<br>";
 
 $resultado = 'Curso de'. $curso . ' versao ' . $versao;
 echo $resultado;
 echo "<br>";
+
+$resultado = sprintf("Curso de %s versão %0.1f", $curso, $versao);
+echo $resultado;
+echo "<br>";
+
+printf("O Curso de %s está na versão %0.1f", $curso, $versao);
+echo "<br>";
+
+$entrada = ['PHP', 7.4];
+$resultado = vsprintf("Curso de %s versão %0.1f", $entrada);
+echo $resultado;
+echo "<br>";
+
+vprintf("Curso de %s versão %0.1f", $entrada);
