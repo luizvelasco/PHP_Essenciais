8c81ab2879c065565af5c5030546fec99a786b63
luizvelasco <luizvelasco@gmail.com>
2023-05-16 08:29:05 -0300

    tratamento de strings

---
+++ funcoes/tratamento-strting.php
@@ -0,0 +1,15 @@
+<?php
+
+var_dump(strtoupper('TreinaWeb Cursos')); //maiúscula
+
+var_dump(strtolower('TreinaWeb Cursos')); //minúscula
+
+var_dump(ucfirst('TreinaWeb cursos')); //primeira em maiúscula
+
+var_dump(ucwords('treinaWeb cursos')); //primeira letra maiúscula
+
+var_dump(ltrim('    TreinaWeb Cursos')); //elimina espaços a esquerda
+
+var_dump(rtrim('TreinaWeb Cursos    ')); //elimina espaços a direita
+
+var_dump(trim('    TreinaWeb Cursos   ')); //elimina todos os espaços
